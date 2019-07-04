c69e1048e0993c41a9b89bb6ef2141099955d84d
William Ahrons <william.ahrons@gmail.com>
2019-07-04 22:18:45 +0200

    Add Nginx

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM java:8
-COPY ["build/libs/standalone-0.1.jar", "."]
 EXPOSE 5555
 ENTRYPOINT ["java","-jar","/standalone-0.1.jar", "--server.port=5555"]
--- src/main/java/com/example/standalone/config/DataSourceConfig.java
+++ src/main/java/com/example/standalone/config/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.example.standalone.config;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
+@Slf4j
 public class DataSourceConfig {
     @Bean
     public DataSource dataSource(DatabaseProperties props) {
@@ -17,7 +19,11 @@ public class DataSourceConfig {
         dataSource.setUrl(props.getUrl());
         dataSource.setUsername(props.getUser());
         dataSource.setPassword(props.getPass());
-
+        log.info("driver: {} , url-db: {}, user: {}, pass: {}",
+                props.getDriver(),
+                props.getUrl(),
+                props.getUser(),
+                props.getPass());
         return dataSource;
     }
 
